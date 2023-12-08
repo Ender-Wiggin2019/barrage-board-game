0f806450ab06ee2c15eca50333505fe7bee822a2
Ender-Wiggin2019 <117010097@link.cuhk.edu.cn>
2023-12-08 14:06:52 +0800

    feat: code clean

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,5 @@
 export * from "@/pages/dashboard/executiveOfficers";
 export * from "@/pages/dashboard/contracts";
 export * from "@/pages/dashboard/externalWorks";
-export * from "@/pages/dashboard/notifications";
 export * from "@/pages/dashboard/create";
 export * from "@/pages/dashboard/TechTiles";
